ea0a19728d169e1ed184e115efb892658597e762
hugosenari <hugosenari@gmail.com>
2022-05-09 20:45:11 -0300

    chore: re index vectors

--- flake.nix
+++ flake.nix
@@ -7,14 +7,6 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
-  inputs."vectors-master".dir   = "master";
-  inputs."vectors-master".owner = "nim-nix-pkgs";
-  inputs."vectors-master".ref   = "master";
-  inputs."vectors-master".repo  = "vectors";
-  inputs."vectors-master".type  = "github";
-  inputs."vectors-master".inputs.nixpkgs.follows = "nixpkgs";
-  inputs."vectors-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
-  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
